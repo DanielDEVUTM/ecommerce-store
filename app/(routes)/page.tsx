b754252f7ca187d7c5d7247d9e85a29b0205ebfc
Crivoi Dan <crivoidan@MacBook-Air-Daniel.local>
2025-09-12 12:40:08 +0300

    2 parte -Environment setup & featured products(am creat cardul pt produse)(Store)

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,25 @@
+import Container from "@/components/ui/container";
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
+export const revalidate = 0;
 
-const HomePage = () => {
+const HomePage =  async () => {
+  const products = await getProducts({isFeatured: true});
+  const billboard  = await getBillboard("6660fb5d-bcba-4697-b663-5890d42f4ee1");
   return (
-    <div>
-      Heloo Store
-    </div>
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard}/>
+
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            <ProductList title="Featured Products" items={products}/>
+          </div>
+        </div>
+
+      </Container>
   );
 }
 export default HomePage;
